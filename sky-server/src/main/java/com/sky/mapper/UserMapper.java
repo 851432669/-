7323aa75f7ce33a2228e41d847d4a65a712ea0ec
Administrator <851432669@qq.com>
2025-04-04 19:25:53 +0800

    Day08 订单下单 and debug user openid问题

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -1,23 +1,28 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.User;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
-    /**
-     * 根据openid查询用户
-     * @param openid
-     * @return
-     */
-    @Select("select * from user where openid=#{openId}")
-    User getByopenid(String openid);
+
 
     /**
      * 插入数据
      * @param user
      */
+    @AutoFill(OperationType.INSERT)
     void insert(User user);
+
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select * from user where openid=#{openid}")
+    User getByOpenId(String openid);
 }
--- sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.sky.constant.MessageConstant;
 import com.sky.dto.UserLoginDTO;
@@ -37,27 +38,26 @@ public class UserServiceImpl implements UserService {
      * @return
      */
     public User wxlogin(UserLoginDTO userLoginDTO) {
-        //调用微信接口服务，获得当前微信用户的openid
+//        调用微信接口服务，获取当前微信用户的Openid
         String openid = getOpenid(userLoginDTO.getCode());
 
-        //判断openid是否为空 如果为空 表示登录失败 抛出业务异常
-        if(openid==null){
+//        判断openId是否为空，如果为空标识登录失败，抛出业务异常
+        if (openid == null) {
             throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
         }
 
-        //判断当前用户是否为新用户
-        User user = userMapper.getByopenid(openid);
-        //如果是新用户 自动完成注册
-        if(user==null){
-            user= User.builder()
+//        判断当前用户是否为新用户
+        User user = userMapper.getByOpenId(openid);
+
+//        如果是新用户,自动完成注册
+        if (user == null) {
+            user = User.builder()
                     .openid(openid)
-                    .createTime(LocalDateTime.now())
-                    .build();
+                    .createTime(LocalDateTime.now()).build();
             userMapper.insert(user);
         }
 
-        //返回这个用户对象
-
+//        返回这个用户对象
         return user;
     }
 
@@ -66,19 +66,17 @@ public class UserServiceImpl implements UserService {
      * @param code
      * @return
      */
-    private String getOpenid(String code){
+    private String getOpenid(String code) {
         //调用微信接口服务，获得当前微信用户的openid
-        Map<String, String> map=new HashMap<>();
-        map.put("appid", weChatProperties.getAppid());
-        map.put("secret", weChatProperties.getSecret());
-        map.put("grant_type", "authorization_code");
+        Map<String, String> map = new HashMap<>();
+        map.put("appid",weChatProperties.getAppid());
+        map.put("secret",weChatProperties.getSecret());
         map.put("js_code",code);
-
+        map.put("grant_type","authorization_code");
         String json = HttpClientUtil.doGet(WX_LOGIN, map);
 
-        JSONObject jsonObject = JSONObject.parseObject(json);
+        JSONObject jsonObject = JSON.parseObject(json);
         String openid = jsonObject.getString("openid");
-
         return openid;
     }
 }
