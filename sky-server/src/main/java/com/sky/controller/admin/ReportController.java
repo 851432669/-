0cbd357c86e56899dff8283fbba41654f95f3396
Administrator <851432669@qq.com>
2025-04-10 19:59:49 +0800

    Day012 Over 后补百度地图 前端补全

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -91,5 +92,15 @@ public class ReportController {
 
         log.info("查询top10数据:{},{}",begin,end);
         return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+    /**
+     * 导出数据
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出数据")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 }
